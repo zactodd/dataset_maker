751e621e6f6fcf7e4bc69c1888fdc299ed3d9a9d
Zac Todd <zactodd0@gmail.com>
2020-10-20 14:21:01 +1300

    Update maker.py

--- dataset_maker/maker.py
+++ dataset_maker/maker.py
@@ -1,5 +1,7 @@
 from abc import ABC, abstractmethod
-from typing import Any
+from typing import Any, Tuple
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 class DatasetMaker(ABC):
@@ -7,17 +9,42 @@ class DatasetMaker(ABC):
         pass
 
     @abstractmethod
-    def make(self, n_samples: int) -> Any:
+    def make(self, n_samples: int) -> Tuple[Any, Any]:
         NotImplementedError()
-        return
+        return "images", "labels"
 
 
-class BBoxsDM(DatasetMaker):
-    def __init__(self, width=640, height=640):
+class SingleSquareBBoxsDM(DatasetMaker):
+    def __init__(self, width=640, height=640, min_width=0, max_width=None):
         super().__init__()
+        assert max_width is None or min_width < max_width, \
+            "min sample width must be smaller than the max sample width."
+        assert max_width is None or height > max_width < width, \
+            "THe max width must be smaller than the width and height of the image."
         self.width = width
         self.height = height
 
+        self.min_width = min_width
+        self.max_width = min(width, height) if max_width is None else max_width
+
+        self._BASE_IMAGE = np.zeros((width, height, 3))
+
     def make(self, n_samples: int):
-        NotImplementedError()
-        return
+        bboxes = []
+        images = []
+        for _ in range(n_samples):
+            image = self._BASE_IMAGE.copy()
+            square_width = np.random.randint(self.min_width, self.max_width)
+
+            x0 = np.random.randint(square_width, self.width - square_width)
+            y0 = np.random.randint(square_width, self.height - square_width)
+            x1 = x0 + square_width
+            y1 = y0 + square_width
+
+            image[x0:x1, y0:y1] = (255, 0, 0)
+
+            bboxes.append(np.asarray([y0, x0, y1, x1]))
+            images.append(image)
+        return images, bboxes
+
+
