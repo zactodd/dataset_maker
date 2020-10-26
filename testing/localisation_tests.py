b03d5d8744899da707ea36c4ccb6dbbe14d917c1
Zac Todd <zactodd0@gmail.com>
2020-10-27 00:23:54 +1300

    Update localisation_tests.py

--- testing/localisation_tests.py
+++ testing/localisation_tests.py
@@ -113,9 +113,6 @@ class TestAnnotation:
             except AssertionError as e:
                 self.verificationErrors.append(str(e))
 
-    def test_fail(self):
-        self.fail()
-
 
 class TestPascalVOC(TestAnnotation, unittest.TestCase):
     def anno_load(self):
