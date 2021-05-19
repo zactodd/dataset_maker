d64565f06ab9bf705072679cc8fab6b01672f552
Zac Todd <zactodd0@gmail.com>
2021-05-20 11:24:09 +1200

    Update instance_segmentation.py

--- dataset_maker/annotations/instance_segmentation.py
+++ dataset_maker/annotations/instance_segmentation.py
@@ -1,9 +1,8 @@
-from dataset_maker.annotations.download_upload import ShardedConvert
+from dataset_maker.annotations.download_upload import LoaderDownloader
 from dataset_maker.patterns import SingletonStrategies, strategy_method
 from abc import ABCMeta, abstractmethod
-from typing import Tuple, List, Generator, Sized
+from typing import Tuple, List
 import numpy as np
-import matplotlib.pyplot as plt
 from dataset_maker import utils
 import json
 import os
@@ -12,8 +11,6 @@ import tensorflow as tf
 from dataset_maker.annotations import dataset_utils, vgg_utils
 import contextlib2
 from PIL import Image
-from collections import defaultdict
-from itertools import count
 
 
 class InstanceSegmentationAnnotationFormats(SingletonStrategies):
@@ -31,7 +28,7 @@ class InstanceSegmentationAnnotationFormats(SingletonStrategies):
 FORMATS = InstanceSegmentationAnnotationFormats()
 
 
-class InstanceSegmentationAnnotation(ShardedConvert, metaclass=ABCMeta):
+class InstanceSegmentationAnnotation(LoaderDownloader, metaclass=ABCMeta):
     """
     Abstract base class for InstanceSegmentationAnnotation as a Loader.
     """
@@ -39,11 +36,13 @@ class InstanceSegmentationAnnotation(ShardedConvert, metaclass=ABCMeta):
         super().__init__()
 
     @staticmethod
+    @abstractmethod
     def load(image_dir: str, annotations_file: str) -> \
             Tuple[List[str], List[np.ndarray], List[np.ndarray], List[np.ndarray], List[np.ndarray]]:
-        return super().load(image_dir, annotations_file)
+        pass
 
     @staticmethod
+    @abstractmethod
     def download(download_dir, image_names, images, bboxes, polygons, classes) -> None:
         pass
 
@@ -59,11 +58,9 @@ class InstanceSegmentationAnnotation(ShardedConvert, metaclass=ABCMeta):
             for idx, (filename, image, bbox_per, poly_per, cls_per) in \
                     enumerate(zip(filenames, images, bboxes, polygons, classes)):
                 # TODO maybe look into different way or find the common standard
-
                 with tf.io.gfile.GFile(f"{image_dir}/{filename}", "rb") as fid:
                     encoded_image = fid.read()
 
-                image = Image.fromarray(np.uint8(image * 255))
                 width, height = image.size
 
                 xmins = []
@@ -148,39 +145,22 @@ class VGG(InstanceSegmentationAnnotation):
     """
 
     @staticmethod
-    def _load_shards(num_shards, image_dir: str, annotations_dir: str, region_label: str = "label") -> Generator:
-        def _load_shard(shard):
-            names = []
-            images = []
-            bboxes = []
-            polygons = []
-            classes = []
-            for annotation in shard.values():
-                filename = annotation["filename"]
-                names.append(filename)
-
-                image = plt.imread(f"{image_dir}/{filename}")
-                images.append(image)
-
-                bboxes_per = []
-                poly_per = []
-                classes_per = []
-
-                regions = annotation["regions"]
-                if isinstance(regions, dict):
-                    regions = regions.values()
-
-                for r in regions:
-                    xs, ys = r["shape_attributes"]["all_points_x"], r["shape_attributes"]["all_points_y"]
-                    bbox = utils.bbox(xs, ys)
-                    bboxes_per.append(np.asarray(bbox))
-                    poly_per.append((xs, ys))
-                    classes_per.append(r["region_attributes"][region_label])
-                bboxes.append(np.asarray(bboxes_per))
-                polygons.append(np.asarray(poly_per))
-                classes.append(np.asarray(classes_per))
-            return names, images, bboxes, polygons, classes
-
+    def load(image_dir: str, annotations_dir: str, region_label: str = "label") -> \
+            Tuple[List[str], List, List[np.ndarray], List[np.ndarray], List[np.ndarray]]:
+        """
+        Loads a VGG file and gets the names, images bounding boxes and classes for thr image.
+        :param image_dir: The directory of where the images are stored.
+        :param annotations_dir: Either a directory of the annotations file or the json annotations file its self.
+        :param region_label: The key that identifies the label being loaded.
+        :return: Returns names, images bounding boxes and classes
+            The names will be a list of strings.
+            The images will be a list of PIL images.
+            The bounding boxes will be a list of np.ndarray with the shape (n, 4) with the coordinates being the
+            format [y0, x0, y1, x1].
+            The classes will be a list of of np.ndarray with the shape (n,) and containing string information.
+        :raise AssertionError: If there is more than one json file in the directory of :param annotations_dir.
+        :raise AssertionError: If there is no json file in the directory of :param annotations_dir.
+        """
         if annotations_dir.endswith(".json"):
             annotations_file = annotations_dir
         else:
@@ -196,19 +176,46 @@ class VGG(InstanceSegmentationAnnotation):
             annotations = json.load(f)
             annotations = vgg_utils.convert_annotations_to_polygon(annotations)
 
-        return (_load_shard(c) for c in utils.chunks(annotations, round(len(annotations) / num_shards)))
+        names = []
+        images = []
+        bboxes = []
+        polygons = []
+        classes = []
+        for annotation in annotations.values():
+            filename = annotation["filename"]
+            names.append(filename)
 
+            images.append(Image.open(filename))
+
+            bboxes_per = []
+            poly_per = []
+            classes_per = []
+
+            regions = annotation["regions"]
+            if isinstance(regions, dict):
+                regions = regions.values()
+
+            for r in regions:
+                xs, ys = r["shape_attributes"]["all_points_x"], r["shape_attributes"]["all_points_y"]
+                bbox = utils.bbox(xs, ys)
+                bboxes_per.append(np.asarray(bbox))
+                poly_per.append((xs, ys))
+                classes_per.append(r["region_attributes"][region_label])
+            bboxes.append(np.asarray(bboxes_per))
+            polygons.append(np.asarray(poly_per))
+            classes.append(np.asarray(classes_per))
+        return names, images, bboxes, polygons, classes
 
     @staticmethod
-    def _process_shard(image_names, images, bboxes, polygons, classes) -> Sized:
-        assert len(image_names) == len(images) == len(bboxes) == len(polygons) == len(classes), \
+    def download(download_dir, image_names, images, bboxes, polygon, classes) -> None:
+        assert len(image_names) == len(images) == len(bboxes) == len(polygon) == len(classes), \
             "The params image_names, images, bboxes, polygons and classes must have the same length." \
             f"len(image_names): {len(image_names)}\n" \
             f"len(images): {len(images)}\n" \
             f"len(bboxes): {len(bboxes)}\n" \
-            f"len(polygons): {len(polygons)}" \
+            f"len(polygons): {len(polygon)}" \
             f"len(classes): {len(classes)}"
-        return {
+        annotations = {
             name: {
                 "regions": [
                     {
@@ -221,22 +228,10 @@ class VGG(InstanceSegmentationAnnotation):
                     for cls, (xs, ys) in zip(classes_per, poly_per)
                 ]
             }
-            for name, image, poly_per, classes_per in zip(image_names, images, polygons, classes)
+            for name, poly_per, classes_per in zip(image_names, polygon, classes)
         }
-
-
-
-    @staticmethod
-    def _combine_shard_output(shards):
-        comb = {}
-        for s in shards:
-            comb.update(s)
-        return s
-
-    @staticmethod
-    def _write(download_dir: str, data: dict) -> None:
         with open(f"{download_dir}/vgg_annotations.json", "w") as f:
-            json.dump(data, f)
+            json.dump(annotations, f)
 
 
 @strategy_method(InstanceSegmentationAnnotationFormats)
@@ -275,45 +270,21 @@ class COCO(InstanceSegmentationAnnotation):
     """
 
     @staticmethod
-    def _load_shards(num_shards, image_dir, annotations_dir) -> Generator:
-        def _load_shard(shard):
-            image_dict = defaultdict(lambda: {"bboxes": [], "classes": [], "polygons": []})
-
-            for name, annotations_per in shard.items():
-                image = plt.imread(f"{image_dir}/{name}")
-                h, w, *_ = image.shape
-
-                image_dict[name]["file_name"] = name
-                image_dict[name]["image"] = image
-
-                for a in annotations_per:
-                    x0, y0, bb_width, bb_height = a["bbox"]
-                    x1, y1 = x0 + bb_width, y0 + bb_height
-                    image_dict[name]["bboxes"].append(np.asarray([y0, x0, y1, x1], dtype="int64"))
-                    image_dict[name]["classes"].append(classes_dict[a["category_id"]])
-
-                    # TODO Implement workflow to allow pycocotools to be installed
-                    if a["iscrowd"]:
-                        raise NotImplementedError()
-                    else:
-                        segmentation = a["segmentation"][0]
-                        poly = segmentation[::2], segmentation[1::2]
-                    image_dict[name]["polygons"].append(poly)
-
-            names = []
-            images = []
-            bboxes = []
-            polygons = []
-            classes = []
-            for info in image_dict.values():
-                name = info["file_name"]
-                names.append(name)
-                images.append(info["image"])
-                bboxes.append(np.asarray(info["bboxes"]))
-                polygons.append(info["polygons"])
-                classes.append(np.asarray(info["classes"]))
-            return names, images, bboxes, polygons, classes
-
+    def load(image_dir: str, annotations_dir: str) ->\
+            Tuple[List[str], List[np.ndarray], List[np.ndarray], List[np.ndarray], List[np.ndarray]]:
+        """
+        Loads a COCO file and gets the names, images bounding boxes and classes for thr image.
+        :param image_dir: THe directory of where the images are stored.
+        :param annotations_dir: Either a directory of the annotations file or the json annotations file its self.
+        :return: Returns names, images bounding boxes and classes
+            The names will be a list of strings.
+            The images will be a list of np.ndarray with the shapes (w, h, d).
+            The bounding boxes will be a list of np.ndarray with the shape (n, 4) with the coordinates being the
+            format [y0, x0, y1, x1].
+            The classes will be a list of of np.ndarray with the shape (n,) and containing string information.
+        :raise AssertionError: If there is more than one json file in the directory of :param annotations_dir.
+        :raise AssertionError: If there is no json file in the directory of :param annotations_dir.
+        """
         if annotations_dir.endswith(".json"):
             annotations_file = annotations_dir
         else:
@@ -329,17 +300,50 @@ class COCO(InstanceSegmentationAnnotation):
             annotations = json.load(f)
 
         classes_dict = {cls_info["id"]: cls_info["name"] for cls_info in annotations["categories"]}
-        image_idx_name = {image_info["id"]: image_info["file_name"] for image_info in annotations["images"]}
 
-        annotations_per_image = defaultdict(list)
-        for a in annotations["annotations"]:
-            annotations_per_image[image_idx_name[a["image_id"]]].append(a)
-        return (_load_shard(c) for c in
-                utils.chunks(annotations_per_image, round(len(annotations_per_image) / num_shards)))
+        image_dict = {
+            image_info["id"]: {
+                "bboxes": [],
+                "classes": [],
+                "polygons": [],
+                "name": image_info["file_name"],
+                "image": Image.OPEN(f"{image_dir}/{image_info['file_name']}")
+            }
+            for image_info in annotations["images"]
+        }
+        for annotation in annotations["annotations"]:
+            idx = annotation["image_id"]
+            x0, y0, bb_width, bb_height = annotation["bbox"]
+            x1, y1 = x0 + bb_width, y0 + bb_height
+            image_dict[idx]["bboxes"].append(np.asarray([y0, x0, y1, x1], dtype="int64"))
+            image_dict[idx]["classes"].append(classes_dict[annotation["category_id"]])
 
+            w, h = image_dict[idx]["image"].size
+
+            # TODO Implement workflow to allow pycocotools to be installed
+            if annotation["iscrowd"]:
+                raise NotImplementedError()
+            else:
+                segmentation = annotation["segmentation"][0]
+                poly = segmentation[::2], segmentation[1::2]
+            image_dict[idx]["polygons"].append(poly)
+
+        names = []
+        images = []
+        bboxes = []
+        polygons = []
+        classes = []
+        for info in image_dict.values():
+            name = info["name"]
+            names.append(name)
+            images.append(info["image"])
+            bboxes.append(np.asarray(info["bboxes"]))
+            polygons.append(info["polygons"])
+            classes.append(np.asarray(info["classes"]))
+        return names, images, bboxes, polygons, classes
 
     @staticmethod
-    def _process_shard(image_names, images, bboxes, polygons, classes) -> Sized:
+    def download(download_dir, image_names, images, bboxes, polygons, classes) -> None:
         assert len(image_names) == len(images) == len(bboxes) == len(polygons) == len(classes), \
             "The params image_names, images, bboxes, polygons and classes must have the same length." \
             f"len(image_names): {len(image_names)}\n" \
@@ -347,64 +351,36 @@ class COCO(InstanceSegmentationAnnotation):
             f"len(bboxes): {len(bboxes)}\n" \
             f"len(polygons): {len(polygons)}" \
             f"len(classes): {len(classes)}"
-        classes_dict = {n: i for i, n in enumerate({cls for classes_per in classes for cls in classes_per}, 1)}
 
-        images_info = {}
+        classes_dict = {n: i for i, n in enumerate({cls for classes_per in classes for cls in classes_per}, 1)}
+        annotation_idx = 0
+        images_info = []
         annotations_info = []
-        classes_info = set()
-        anno_idx = count()
         for img_idx, (name, image, bboxes_per, poly_per, classes_per) in \
                 enumerate(zip(image_names, images, bboxes, polygons, classes), 1):
-            h, w, _ = image.shape
-
-            images_info[str(name)] = {"id": img_idx, "file_name": str(name), "width": int(w), "height": int(h)}
-
+            w, h = image.size
+            images_info.append({"id": img_idx, "file_name": str(name), "width": int(w), "height": int(h)})
             for (y0, x0, y1, x1), (xs, ys), cls in zip(bboxes_per, poly_per, classes_per):
                 annotations_info.append({
-                    "id": next(anno_idx),
-                    "image_id": str(name),
-                    "category_id": str(classes_dict[cls]),
+                    "id": annotation_idx,
+                    "image_id": img_idx,
+                    "category_id": classes_dict[cls],
                     "iscrowd": 0,
                     "segmentation": [[int(p) for ps in zip(xs, ys) for p in ps]],
                     "bbox": [int(x0), int(y0), int(x1 - x0), int(y1 - y0)],
                     "area": int(utils.bbox_area(y0, x0, y1, x1))
                 })
-                classes_info |= set(classes_per)
-        return {
+                annotation_idx += 1
+
+        data = {
             "images": images_info,
             "annotations": annotations_info,
-            "categories": [{"id": int(cat_idx), "name": str(cls), "supercategory": "type"}
-                           for cls, cat_idx in classes_dict.items()]
+            "categories": [{
+                "id": int(cat_idx),
+                "name": str(cls),
+                "supercategory": "type"
+            } for cls, cat_idx in classes_dict.items()]
         }
-
-    @staticmethod
-    def _combine_shard_output(shards):
-        cls_idx, img_idx, anno_idx = count(), count(), count()
-
-        # TODO work out if slow
-        categories = {cls["name"]: {"id": next(cls_idx), "name": str(cls), "supercategory": "type"}
-                      for s in shards for cls in s["categories"]}
-        image_info = {info["file_name"]: {"id": next(img_idx), **info}
-                      for s in shards for info in s["images"].values()}
-
-        annotations_info = []
-        for s in shards:
-            local_cls_idx = {cls["id"]: categories[cls["name"]]["id"] for cls in s["categories"]}
-            local_img_idx = {info["id"]: image_info[info["file_name"]]["id"] for info in s["images"]}
-            for a in s["annotations"]:
-                a["category_id"] = local_cls_idx[a["category_id"]]
-                a["image_id"] = local_img_idx[a["image_id"]]
-                a["id"] = next(anno_idx)
-                annotations_info.append(a)
-
-        return {
-            "images": list(image_info.values()),
-            "annotations": annotations_info,
-            "categories": list(categories.values())
-        }
-
-    @staticmethod
-    def _write(download_dir: str, data: dict) -> None:
         with open(f"{download_dir}/coco_annotations.json", "w") as f:
             json.dump(data, f)
 
